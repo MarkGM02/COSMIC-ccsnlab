simplified + bug fix for neutrino loss and ejecta calculation

src/ccsnlab/data_loading/delayed_fryer.py
@@ -1,6 +1,6 @@
-#Solving the delayed fryer remnant mass prescription for the original CO core mass
+"""No longer used in this package, left in as a python implementation of the Delayed Fryer+12 prescription"""
 
-import numpy as np
+from ccsnlab.data_loading.neutrino import neutrino_mass_loss
 
 def get_proto_core_mass(core_mass):
     """
@@ -18,23 +18,6 @@ def get_proto_core_mass(core_mass):
     else:
         return 1.6
 
-def neutrino_mass_loss(remnant_mass, rembar_massloss=0.5):
-    """
-    Apply COSMIC's neutrino mass loss prescription, where the mass loss is
-    limited to rembar_massloss. If this is positive, it is an absolute mass limit.
-    If negative, it is a fraction of the remnant mass.
-    
-    :param remnant_mass: The mass of the remnant before neutrino mass loss is applied.
-    :param rembar_massloss: The maximum mass loss due to neutrinos. If positive, this is an absolute mass limit. If negative, this is a fraction of the remnant mass.
-    :return: The mass of the remnant after neutrino mass loss is applied.
-    """
-    reduced_remnant_mass = 6.6666667*(np.sqrt(1.0 + 0.3* remnant_mass) - 1.0)
-    mass_diff = remnant_mass - reduced_remnant_mass
-
-    if rembar_massloss >= 0:
-        return remnant_mass - rembar_massloss if mass_diff > rembar_massloss else reduced_remnant_mass
-    else:
-        return remnant_mass * (1 + rembar_massloss) if mass_diff > -rembar_massloss * remnant_mass else reduced_remnant_mass
 
 def get_remnant_mass(core_mass, total_mass, rembar_massloss=0.5):
     """